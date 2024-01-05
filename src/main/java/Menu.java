7f1a3fd2d9ce59e91b5b583b0096d5c2d4fc3b27
farzadafi <farzadafi40@gmail.com>
2024-01-05 15:05:03 +0330

    add new swith case to publicMenu and create signUp menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,10 +1,15 @@
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Menu {
 
     private final Scanner scanner = new Scanner(System.in);
+    private final UserService userService = new UserService();
 
-    public void publicMenu() {
+    public Menu() throws SQLException {
+    }
+
+    public void publicMenu() throws SQLException {
         System.out.println("***** WELCOME *****");
         System.out.println("1-SIGN UP");
         System.out.println("2-SIGN IN");
@@ -13,5 +18,15 @@ public class Menu {
 
         int number = scanner.nextInt();
         scanner.nextLine();
+
+        switch (number) {
+            case 1 -> signUp();
+            case 2 -> System.out.println("signUp");
+            case 3 -> System.out.println("exit");
+        }
+    }
+
+    public void signUp() throws SQLException {
+        userService.singUp();
     }
 }
