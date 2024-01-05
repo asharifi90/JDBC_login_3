4be8fd025b180f360ad2e517399380cabf81635a
farzadafi <farzadafi40@gmail.com>
2024-01-05 14:31:10 +0330

    add no args and all args construector

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,6 +4,13 @@ public class User {
     private String username;
     private String password;
 
+    public User() {
+    }
 
+    public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
 }
-
