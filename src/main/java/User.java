01ef83202d5b35cfd71994d191c679528b967931
farzadafi <farzadafi40@gmail.com>
2024-01-05 14:32:43 +0330

    add toString method

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -45,4 +45,15 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+               "firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", username='" + username + '\'' +
+               ", password='" + password + '\'' +
+               '}';
+    }
 }
+
