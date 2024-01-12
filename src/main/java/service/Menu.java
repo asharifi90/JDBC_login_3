b8229fae54ef0fe2f506f2fd1cad6b06b542515f
asharifi90 <asharifibb@gmail.com>
2024-01-12 11:29:31 +0330

    add to service package

--- src/main/java/service/Menu.java
+++ src/main/java/service/Menu.java
@@ -1,10 +1,14 @@
+package service;
+
+import utility.ApplicationContext;
+
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Menu {
 
     private final Scanner scanner = new Scanner(System.in);
-    private final UserService userService = new UserService();
+    private final UserService userService = ApplicationContext.getUserService();
 
     public Menu() throws SQLException {
     }
