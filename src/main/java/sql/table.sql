5b2f56a600ea259a94aeb895d95109637ada477b
asharifi90 <asharifibb@gmail.com>
2024-01-12 10:49:20 +0330

    add national id to sql table

--- src/main/java/sql/table.sql
+++ src/main/java/sql/table.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
+    national_id VARCHAR(255),
     username VARCHAR(255),
     password VARCHAR(255)
 );
