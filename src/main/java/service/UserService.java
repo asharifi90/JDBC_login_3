f6f7c7ce33a5f5568f7d9d515b0e4b20eaa1daf9
asharifi90 <asharifibb@gmail.com>
2024-01-12 11:17:39 +0330

    add national id validation methode

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,12 +1,20 @@
+package service;
+
+import model.User;
+import repository.UserRepository;
+import utility.Validation;
+
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class UserService {
 
     private final Scanner scanner = new Scanner(System.in);
-    private final UserRepository userRepository = new UserRepository();
+    private final UserRepository userRepository;
 
-    public UserService() throws SQLException {
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public void singUp() throws SQLException {
@@ -16,15 +24,26 @@ public class UserService {
         System.out.println("Please enter your lastname:");
         String lastname = scanner.nextLine();
 
+        String nationalId = "";
+        while (true) {
+            System.out.println("Please Enter your national ID");
+            nationalId = scanner.nextLine();
+            if (Validation.validateMelliCode(nationalId)) {
+                break;
+            } else {
+                System.out.println("Enter the correct form of national ID");
+            }
+        }
+
         System.out.println("Please enter your username:");
         String username = scanner.nextLine();
 
         System.out.println("Please enter your password:");
         String password = scanner.nextLine();
 
-        User user = new User(firstname, lastname, username, password);
+        User user = new User(firstname, lastname, nationalId, username, password);
         int result = userRepository.registerUser(user);
-        if(result == 1)
+        if (result == 1)
             System.out.println(firstname + " you successfully register :)");
         else
             System.out.println("something is wrong :|");
@@ -38,11 +57,11 @@ public class UserService {
         String password = scanner.nextLine();
 
         User user = userRepository.findByUsername(username);
-        if(user == null)
+        if (user == null)
             System.out.println("Please register first");
-        else if(!user.getPassword().equals(password))
+        else if (!user.getPassword().equals(password))
             System.out.println("Please enter correct password");
         else
-        System.out.println("WELCOME " + user.getUsername());
+            System.out.println("WELCOME " + user.getUsername());
     }
 }
